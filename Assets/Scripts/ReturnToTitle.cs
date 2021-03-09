90698fe6fdae42d4801f3b11ba345e5c07443399
YoshiTa0527 <dfingao0527@gmail.com>
2021-03-09 19:41:55 +0900

    vee用にとりあえずクリア後にタイトル画面に戻るボタンを追加した

---
+++ Assets/Scripts/ReturnToTitle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// ゲーム中に呼び出すLoadSceneManager
+/// </summary>
+public class ReturnToTitle : MonoBehaviour
+{
+    /// <summary>ロードするシーン名</summary>
+    [SerializeField] string m_loadSceneName = null;
+
+    public void OnReturnToTitle()
+    {
+        SceneManager.LoadScene(m_loadSceneName);
+    }
+
+}
+
+
